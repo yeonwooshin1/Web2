33b635ecc2d8a8974e6a6fc346ef57a3d1348732
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-01 09:49:41 +0900

    250901 - web2 커밋푸시

---
+++ src/main/java/example/day01/AppStart.java
@@ -0,0 +1,13 @@
+package example.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStart { // class start
+
+    public static void main(String[] args) {    // main start
+        SpringApplication.run(AppStart.class);
+    }   // main end
+
+}   // class end
