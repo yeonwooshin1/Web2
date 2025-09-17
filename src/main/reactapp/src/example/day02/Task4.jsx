a2d067b5185882aefcd4b8109406de256b1b2416
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-17 18:11:56 +0900

    250917 - 오전 오후 수업 커밋

--- src/main/reactapp/src/example/day02/Task4.jsx
+++ src/main/reactapp/src/example/day02/Task4.jsx
@@ -5,29 +5,34 @@ export default function Task4( props ){
     const [ userName , setUserName ] = useState('');
     const [ number , setNumber ] = useState('');
     const [ userAge , setUserAge ] = useState('');
+
     // 그걸 담아 보여줄 리스트
     const [ userNumberList , setUserNumberList ] = useState( [] );
+
     // 리스트 추가
     const NumberListAdd = () => {
         const newObj = { id: userNumberList.length + 1,  userName , number , userAge };
+        // ... 복사 후 붙이기
         setUserNumberList([...userNumberList , newObj]);
     }   // func end
+
     // 리스트 삭제
     const deleteList = ( id ) => {
         setUserNumberList(userNumberList.filter( (item) => { return item.id !== id } ) );
     }   // func end
 
-
+    // 출력 return 부
     return (<>
+
     <h2>전화번호부</h2>
-    {/* 입력 + 등록 폼 */}
+    {/* 입력, 등록 폼 */}
     <div>
         <input placeholder="성명"   value={ userName } onChange={(e) => {setUserName(e.target.value)}} />
         <input placeholder="연락처(예 : 010-1234-5678)" value={ number } onChange={(e) => {setNumber(e.target.value)}} />
         <input placeholder="나이" value={ userAge } onChange={(e) => {setUserAge(e.target.value)}} />
         <button onClick={NumberListAdd}> 등록 </button>
     </div>
-    {/* 리스트 폼 + 삭제 */}
+    {/* 리스트 폼, 삭제버튼 */}
     <ul>
         {
             userNumberList.map((item) => {
@@ -41,5 +46,6 @@ export default function Task4( props ){
         }
     </ul>
     총{userNumberList.length}명
+    
     </>)
 } // func end 
